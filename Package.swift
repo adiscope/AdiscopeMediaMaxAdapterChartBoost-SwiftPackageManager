8fad1b9300699e6cbb121a2c36b5379d32319c8b
kyungboshim <kyungbo.shim@neowiz.com>
2025-05-22 17:51:52 +0900

    4.3.0 배포

--- Package.swift
+++ Package.swift
@@ -10,12 +10,15 @@ let package = Package(
             name: "AdiscopeMediaMaxAdapterChartBoost",
             targets: ["AdiscopeMediaMaxAdapterChartBoostTarget"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/adiscope/Adiscope-iOS-ChartBoost.git", exact: "4.3.0"),
+    ],
     targets: [
         .target(
             name: "AdiscopeMediaMaxAdapterChartBoostTarget",
             dependencies: [
                 .target(name: "AdiscopeMediaMaxAdapterChartBoost"),
-                .target(name: "ChartboostSDK"),
+                .product(name: "AdiscopeWithChartBoost", package: "Adiscope-iOS-ChartBoost"),
             ],
             path: "Sources"
         ),
@@ -23,11 +26,6 @@ let package = Package(
             name: "AdiscopeMediaMaxAdapterChartBoost",
             url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/4.3.0/AdiscopeMediaMaxAdapterChartBoost.zip",
             checksum: "d67843bd6b519e515af570e9a724677a1b556a75631c6348149a68fdd4d3a86c"
-        ),
-        .binaryTarget(
-            name: "ChartboostSDK",
-            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/4.2.0/ChartboostSDK.xcframework.zip",
-            checksum: "768902d29c78c1dc8d7ea1da58ddd488bf33b12ef71003e9d5048bba6d60db12"
         ),
     ]
 )
