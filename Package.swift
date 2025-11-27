aac8b1196f3e98ec60b56cbe7e75149b9bd42b3d
kyungboshim <kyungbo.shim@neowiz.com>
2025-11-27 15:39:42 +0900

    5.0.0 배포

--- Package.swift
+++ Package.swift
@@ -11,21 +11,27 @@ let package = Package(
             targets: ["AdiscopeMediaMaxAdapterChartBoostTarget"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/adiscope/Adiscope-iOS-ChartBoost.git", exact: "4.3.0"),
+        .package(url: "https://github.com/adiscope/Adiscope-iOS-ChartBoost.git", exact: "5.0.0"),
     ],
     targets: [
         .target(
             name: "AdiscopeMediaMaxAdapterChartBoostTarget",
             dependencies: [
                 .target(name: "AdiscopeMediaMaxAdapterChartBoost"),
+                .target(name: "AdiscopeMediaMaxAdapterChartboostMediationAdapter"),
                 .product(name: "AdiscopeWithChartBoost", package: "Adiscope-iOS-ChartBoost"),
             ],
             path: "Sources"
         ),
         .binaryTarget(
             name: "AdiscopeMediaMaxAdapterChartBoost",
-            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/4.4.1/AdiscopeMediaMaxAdapterChartBoost.zip",
-            checksum: "d1ced248e0aae2d94fdd27619408f35e79509a32a4ab40650a602b57dadc0262"
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/5.0.0/AdiscopeMediaMaxAdapterChartBoost.zip",
+            checksum: "6691b581ab1f1451bdf405bf41a59363b5a84e191bd0c785644be1b1391e6267"
+        ),
+        .binaryTarget(
+            name: "AdiscopeMediaMaxAdapterChartboostMediationAdapter",
+            url: "https://github.com/adiscope/Adiscope-iOS-Sample/releases/download/5.0.0/AppLovinMediationChartboostAdapter.xcframework.zip",
+            checksum: "5517e9b527fe1d33e4ba7303319a09804b1245f1311c81e609df9a6daaa39010"
         ),
     ]
 )
